343ece7474280953cfdd390683576bfbd8d7b9b4
KariKorpinen <karikkorpinen@gmail.com>
2018-01-30 23:51:38 +0200

    osa1teht8

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,62 +2,96 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
- constructor() {
-    super()
+ constructor(props) {
+    super(props)
     this.state = {
-      counter: 0,
-      counter2: 0,
-      counter3: 0
+      vasen: 0,
+      oikea: 0,
+      keski: 0,
+      kaikki: [],
     }
   }
 
-  asetaArvoon = (arvo) => {
-    return () => {
-      this.setState({ counter: arvo })
-    }
+  klikVasen = () => {
+    this.setState({
+      vasen: this.state.vasen + 1,
+      kaikki: this.state.kaikki.concat('v'),
+    })
   }
 
-  asetaArvoon2 = (arvo) => {
-    return () => {
-      this.setState({ counter2: arvo })
-    }
+  klikKeski = () => {
+    this.setState({
+      keski: this.state.keski + 1,
+      kaikki: this.state.kaikki.concat('k'),
+    })
   }
 
-  asetaArvoon3 = (arvo) => {
-    return () => {
-      this.setState({ counter3: arvo })
-    }
+  klikOikea = () => {
+    this.setState({
+      oikea: this.state.oikea + 1,
+      kaikki: this.state.kaikki.concat('o'),
+    })
   }
 
-
+  
+  
   render() {
+    const historia = () => {
+      if (this.state.kaikki.length === 0) {
+         return (
+            <div>
+               <em>sovellusta käytetään nappeja painelemalla</em>
+            </div>
+         )
+       }
+       return (
+          <div>
+            näppäilyhistoria: {this.state.kaikki.join(' ')}
+          </div>
+       )
+    }
+    const pituus = () => this.state.kaikki.length
+    const vasen = () => this.state.vasen
+    const keski = () => this.state.keski
+    const oikea = () => this.state.oikea
     return (
       <div>
         <h1>anna palautetta</h1>
-        
-        <div>
-           <button onClick={this.asetaArvoon(this.state.counter + 1)}>
-            hyvä
-          </button>
-          <button onClick={this.asetaArvoon2(this.state.counter2 + 1)}>
-            neutraali
-          </button>
-           <button onClick={this.asetaArvoon3(this.state.counter3 + 1)}>
-            huono
-           </button>
-        </div>
+        <button onClick={this.klikVasen}>hyvä</button>
+        <button onClick={this.klikKeski}>neutraali</button>
+        <button onClick={this.klikOikea}>huono</button>
+        <div>{historia()}</div>
         <h1>statistiikka</h1>
-        <div>hyvä {this.state.counter}</div>
-        <div>neutraali {this.state.counter2}</div>
-        <div>huono {this.state.counter3}</div>
-        <div>keskiarvo {((this.state.counter * 1) + (this.state.counter2 * 0) + (this.state.counter3 * -1))/(this.state.counter + this.state.counter2 + this.state.counter3) }</div>
-        <div>positiivisia {this.state.counter /(this.state.counter + this.state.counter2 + this.state.counter3) * 100 } % </div>
-
-
-      </div>
+        
+        <div>yhteensä {pituus()}</div>
+        <div>hyvä {vasen()}</div>
+        <div>neutraaleja {keski()}</div>
+        <div>huono {oikea()}</div>
+        <Statistics vasen={this.state.vasen} keski={this.state.keski} oikea={this.state.oikea} />
+        
+        </div>
+        
      )
    }
 }
+
+const Statistics = (props) => {
+     return(
+        <div>
+           <Statistic sta={((props.vasen * 1) + (props.keski * 0) + (props.oikea * -1))/(props.vasen + props.keski + props.oikea) } />
+           <Statistic sta={props.vasen /(props.vasen + props.keski + props.oikea) * 100 } /> 
+        </div>
+     )
+  }
+  const Statistic = (props) => {
+    return(
+      <div>
+         <p>{props.sta}</p>
+      </div>
+   )
+ }
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
